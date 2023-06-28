2153570e5b43aa2230178b57f87ead94287f2366
ElzaMr <elza-mr@mail.ru>
2023-06-28 22:54:22 +0300

    fix Dao getListOfCar()

--- src/main/java/web/Dao/CarDaoImp.java
+++ src/main/java/web/Dao/CarDaoImp.java
@@ -10,15 +10,28 @@ import java.util.List;
 @Component
 public class CarDaoImp implements CarDao{
 
-    private List<Car>listOfCars = new ArrayList<>();
-
-    @Override
-    public List<Car> getListOfCar(int count) {
+    private final List<Car>listOfCars = new ArrayList<>();
+    {
         listOfCars.add(new Car("BMW",2,"Masha"));
         listOfCars.add(new Car("Opel",6,"Misha"));
         listOfCars.add(new Car("Mercedes",2,"Max"));
         listOfCars.add(new Car("Toyota",22,"Masha"));
         listOfCars.add(new Car("Niva",2,"Vladimir"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CarDaoImp().getListOfCar(-5));
+
+    }
+
+
+
+    @Override
+    public List<Car> getListOfCar(int count) {
+        if (count >= 5) {
+            return listOfCars;
+        }
         return listOfCars.stream().limit(count).toList();
     }
+
 }
