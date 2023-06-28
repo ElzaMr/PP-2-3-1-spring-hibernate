f2243565c6a6c9695b1d8b2ae3dc8752ffa6fd2d
ElzaMr <elza-mr@mail.ru>
2023-06-28 13:55:26 +0300

    fix

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -39,7 +39,7 @@ public class Car {
     }
 
     @Override
-    public String toString() {
+    public String  toString() {
         return "Car{" +
                 "model='" + model + '\'' +
                 ", series=" + series +
