93379b3cceec382926aa8b2fb360f994dd4ee53f
ElzaMr <elza-mr@mail.ru>
2023-06-29 16:21:45 +0300

    add class User

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,17 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "age")
+    private int age;
+}
